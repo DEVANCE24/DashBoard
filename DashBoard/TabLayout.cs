c9ec0946c02f78bcf2e23b969b0a3e06f031be35
devansh.desai <devansh.desai@1R-77>
2022-03-17 17:06:17 +0530

    Add project files.

---
+++ DashBoard/TabLayout.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.BottomAppBar;
+using Google.Android.Material.BottomNavigation;
+
+namespace DashBoard
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {      
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.tablayout);
+            BottomAppBar bottomAppBar = FindViewById<BottomAppBar>(Resource.Id.bottomAppBar);
+
+            
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        
+        
+    }
+}
